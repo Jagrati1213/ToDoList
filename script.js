c02dc3002a8f8f975e2c97753c73c608fb3803ad
jagrati gupta <123jagratigupta@gmail.com>
2021-12-07 20:27:06 +0530

    add Edit Button

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ addItems.addEventListener("click", () => {
     taskcontainer.innerHTML += `
            <div class="task-box">
            <p class="task-input">${taskInput.value}</p>
-           <button class="deletebtn"><i class="fas fa-trash-alt"></i></button>
+           <button class="deletebtn">Del</button>
+           <button class="editbtn">Edit</button>
            </div>`;
 
     taskInput.value = "";
@@ -29,16 +30,54 @@ addItems.addEventListener("click", () => {
     });
 
     // after work done
-    var taskDones = document.querySelectorAll(".add-items .task-box");
+    var taskDones = document.querySelectorAll(".add-items .task-input");
     taskDones.forEach(taskDone => {
       taskDone.addEventListener("click", () => {
         taskDone.classList.toggle("done");
       });
     });
 
-  }
+    //Edit the task 
+    var taskEdits = document.querySelectorAll(".add-items .editbtn");
+    taskEdits.forEach(taskEdit => {
+      taskEdit.addEventListener("click", (e) => {
 
+         let button = e.target;
+         let taskBox = button.parentNode;
+         let pera =taskBox.firstElementChild;
+
+         if (button.innerHTML === "Edit") {
+
+          let creatInput = document.createElement("input");
+          creatInput.setAttribute("type", "text");
+          creatInput.value = pera.innerHTML;
+          taskBox.insertBefore(creatInput, pera);
+          taskBox.removeChild(pera)
+          button.innerHTML = "Save";
+      }
+
+      //save the edit ...
+      else if (button.innerHTML === "Save") {
+
+          let input = taskBox.firstElementChild;
+
+          let creatPera = document.createElement("p");
+          creatPera.className = "task-input";
+          creatPera.innerHTML = input.value;
+          taskBox.insertBefore(creatPera, input);
+          taskBox.removeChild(input)
+          button.innerHTML = "Edit";
+      }
+      //---------- edit Event end here --------//
+  });
+  //------------- edit ForEach end -----------//
+  });
+  // ---------- Else statement end -----------//
+  }
+  //----- addBtn Listner end here ----//
 });
+
+
 
 // delete the given tasks
 var dels = document.querySelectorAll(".add-items .deletebtn");
@@ -47,10 +86,46 @@ dels.forEach(del => {
     del.parentNode.remove();
   });
 });
+
+
 // given work done
-var taskDones = document.querySelectorAll(".add-items .task-box");
+var taskDones = document.querySelectorAll(".add-items .task-pera");
 taskDones.forEach(taskDone => {
   taskDone.addEventListener("click", () => {
     taskDone.classList.toggle("done");
   });
+});
+
+//given task edit 
+var taskEdits = document.querySelectorAll(".add-items .editbtn");
+taskEdits.forEach(taskEdit => {
+  taskEdit.addEventListener("click", (e) => {
+
+     let button = e.target;
+     let taskBox = button.parentNode;
+     let pera =taskBox.firstElementChild;
+
+     if (button.innerHTML === "Edit") {
+
+      let creatInput = document.createElement("input");
+      creatInput.setAttribute("type", "text");
+      creatInput.value = pera.innerHTML;
+      taskBox.insertBefore(creatInput, pera);
+      taskBox.removeChild(pera)
+      button.innerHTML = "Save";
+  }
+
+  //save the edit ...
+  else if (button.innerHTML === "Save") {
+
+      let input = taskBox.firstElementChild;
+
+      let creatPera = document.createElement("p");
+      creatPera.className = "task-input";
+      creatPera.innerHTML = input.value;
+      taskBox.insertBefore(creatPera, input);
+      taskBox.removeChild(input)
+      button.innerHTML = "Edit";
+  }
+});
 });
